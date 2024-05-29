a4cd41d4536e65d240a86d2bca47ebc19e36d774
Cristian Varela Casas <cristianchantada@gmail.com>
2024-05-29 15:20:05 +0200

    Falta darle una revisión en casa

---
+++ src/app/countries/countries.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { SelectorPagesComponent } from './pages/selector-pages/selector-pages.component';
+import { CountriesRoutingModule } from './countries-routing.module';
+import { ReactiveFormsModule } from '@angular/forms';
+
+
+@NgModule({
+  declarations: [
+    SelectorPagesComponent
+  ],
+  imports: [
+    CommonModule,
+		CountriesRoutingModule,
+		ReactiveFormsModule
+  ]
+})
+export class CountriesModule { }
